cd105e7626335476928076f9751e51be486028c7
VeldigSinnaMann <notsoanonym@gmail.com>
2025-12-26 18:06:32 +0100

    C: refactor, function files

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #main for bookbott
-
+from stats import get_book_words
 
 #get text function
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
     
-def get_book_words(book_text):
-    words_in_book = book_text.split()
-    num_words = len(words_in_book) 
-    return num_words
-
 def main():
     print(f"Found {get_book_words(get_book_text("books/frankenstein.txt"))} total words")
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_book_words(book_text):
+    words_in_book = book_text.split()
+    num_words = len(words_in_book) 
+    return num_words
