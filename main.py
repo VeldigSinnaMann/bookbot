74a4074cf586d76f0dbeec8810b9fc033c8efa0b
VeldigSinnaMann <notsoanonym@gmail.com>
2026-01-02 03:22:48 +0100

    fully don

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 #main for bookbott
+import sys
 from stats import get_book_words
 from stats import get_letters_used
-
+from stats import sort_on
+from stats import sort_dictionaries
+from stats import print_report
 #get text function
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
     
 def main():
-    string_book = get_book_text("books/frankenstein.txt")
-    print(f"Found {get_book_words(string_book)} total words")
-    letters_used = get_letters_used(string_book)
-    print(letters_used)
+    if len(sys.argv) == 2:
+        file_path = sys.argv[1]
+        string_book = get_book_text(file_path)
+        word_count = get_book_words(string_book)
+        letters_used = get_letters_used(string_book)
+        sorted_ch = sort_dictionaries(letters_used)
+        print_report(file_path, word_count, sorted_ch)
+    else:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,26 @@ def get_letters_used(book_list):
                 letters_used[ch] =letters_used.get(ch, 0) + 1
     return letters_used
 
+def sort_on(item):
+    return item["num"]
+
+
+def sort_dictionaries(dictionary):
+    character_count = [] 
+    for char, number in dictionary.items():
+        character_count.append({"char": char, "num": number})
+    character_count.sort(reverse=True , key=sort_on)
+    return character_count
+
+
+def print_report(filepath, wordcount, sorted_ch):
+    print(
+          "============ BOOKBOT ============\n"
+          f"Analyzing book found at {filepath}...\n"
+          "----------- Word Count ----------\n"
+          f"Found {wordcount} total words\n"
+          "--------- Character Count -------"
+          )
+    for ch in sorted_ch:
+        print(f"{ch['char']}: {ch['num']}")
+    print("============= END ===============")
